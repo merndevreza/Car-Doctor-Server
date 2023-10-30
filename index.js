a1de4036a73d94b1c6bcff192d4e8237bd05778b
merndevreza <merndevreza@gmail.com>
2023-10-30 06:15:56 +0600

    get single service by _id

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -43,7 +43,12 @@ async function run() {
       const cursor = servicesCollection.find();
       const result = await cursor.toArray();
       res.send(result)
-
+    })
+    app.get("/service/:id",async(req,res)=>{
+      const id = req.params;
+      const query ={_id:new ObjectId(id)}
+      const result = await servicesCollection.findOne(query)
+      res.send(result)
     })
 
     // Send a ping to confirm a successful connection
